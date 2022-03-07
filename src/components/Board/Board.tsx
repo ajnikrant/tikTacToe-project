37eb89d9b2c86e25c6c8096d241c2c7e4f08a2a5
Amanda <ajnikrant@gmail.com>
2022-03-07 16:42:07 -0500

    created row component, added three cells to a row and three rows to the board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { Row } from '../Row'
+ 
 export function Board() {
+
+    // const rowMapper = (n: integer) => {
+    //     for (let i=0; i<n; i++){
+    //         <Row key={i} />
+    //     }
+    // }
   return (
     <div>
         <h2>THIS IS THE BOARD</h2>
+        {/* <div>{rowMapper(3)}</div> */}
+        <div><Row /></div>
+        <div><Row /></div>
+        <div><Row /></div>
     </div>
   );
 }
---
+++ src/components/Row/Row.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Cell } from '../Cell';
+
+export function Row() {
+  return (
+    <div>
+        <Cell />
+        <Cell />
+        <Cell />
+    </div>
+  );
+}
